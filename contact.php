ab3181013678caec813e04783118d41cfeb9f575
Mixa PC <simonovicyt@gmail.com>
2025-03-18 21:40:15 +0100

    Sve znas brat moj

--- contact.php
+++ contact.php
@@ -86,28 +86,7 @@ mysqli_close($conn);
 
 
 
-<!DOCTYPE html>
-
-<html lang="en">
-
-<head>
-
-  <meta charset="UTF-8">
-
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <title>Ketra Tech</title>
-
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
-  <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
-
-  <link rel="stylesheet" href="styles.css">
-
-</head>
-
-<body>
-
+<?php include_once('header.php'); ?>
 
 
 <div class="loader loader--active">
@@ -144,7 +123,7 @@ mysqli_close($conn);
 
 <div class="main">   
 
-     <video autoplay muted loop>
+     <video autoplay muted loop disablePictureInPicture> 
 
       <source src="img/gradient.webm" type="video/mp4">
 
@@ -153,62 +132,6 @@ mysqli_close($conn);
     </video>
 
   </div>
-
-
-
-
-
-  <nav class="navbar navbar-expand-lg">
-
-    <div class="container">
-
-        <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt="Logo"></a>
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-
-            <span class="navbar-toggler-icon"></span>
-
-        </button>      
-
-        <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
-
-            <ul class="navbar-nav mx-auto">
-
-                <li class="nav-item">
-
-                    <a class="nav-link" href="services.php" data-text="service">Services</a>
-
-                </li>
-
-                <li class="nav-item">
-
-                    <a class="nav-link" href="contact.php" data-text="contact">Contact</a>
-
-                </li>
-
-                <li class="nav-item">
-
-                    <a class="nav-link" href="aboutus.php" data-text="about us">About us</a>
-
-                </li>
-
-                <li class="nav-item">
-
-                    <a class="nav-link" href="careers.php" data-text="careers">Careers</a>
-
-                </li>
-
-            </ul>
-
-            <a href="hirenow.php"><button class="schedule">Hire Now</button></a>
-
-        </div>
-
-    </div>
-
-</nav>
-
-
 
   <div class="container py-5">
 
--- script.js
+++ script.js
@@ -173,6 +173,168 @@
 
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+
+  const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+
+
+  accordionHeaders.forEach((header) => {
+
+    const collapseTargetId = header.getAttribute("data-bs-target");
+
+    const collapseTarget = document.querySelector(collapseTargetId);
+
+
+
+    collapseTarget.addEventListener("shown.bs.collapse", function () {
+
+      const icon = header.querySelector(".icon");
+
+      icon.textContent = "−";
+
+    });
+
+
+
+    collapseTarget.addEventListener("hidden.bs.collapse", function () {
+
+      const icon = header.querySelector(".icon");
+
+      icon.textContent = "+";
+
+    });
+
+  });
+
+});
+
+
+
+
+
+
+function checkVisibility() {
+
+    const boxes = document.querySelectorAll('.box');
+
+    const windowHeight = window.innerHeight;
+
+
+
+    boxes.forEach((box, index) => {
+
+        const boxTop = box.getBoundingClientRect().top;
+
+
+
+        if (boxTop <= windowHeight * 0.8) { 
+
+            setTimeout(() => {
+
+                box.classList.add('visible');
+
+            }, index * 200); 
+
+        }
+
+    });
+
+}
+
+
+
+window.addEventListener('scroll', checkVisibility);
+
+
+
+checkVisibility();
+
+
+
+
+window.addEventListener('scroll', () => {
+
+if (window.scrollY > lastScrollY) {
+
+  navbar.classList.add('hide');
+
+  navbar.classList.remove('show');
+
+} else {
+
+  navbar.classList.add('show');
+
+  navbar.classList.remove('hide');
+
+}
+
+lastScrollY = window.scrollY;
+
+});
+
+
+
+  element.addEventListener("mouseover", () => {
+
+    let iteration = 0;
+
+    let interval = setInterval(() => {
+
+      element.textContent = originalText
+
+        .split("")
+
+        .map((char, index) => {
+
+          if (index < iteration) return char;
+
+          return randomCar.charAt(Math.floor(Math.random() * randomCar.length));
+
+        })
+
+        .join("");
+
+
+
+      if (iteration >= originalText.length) {
+
+        clearInterval(interval);
+
+      }
+
+      iteration++;
+
+    }, 100);
+
+  });
+
+
+var loader = document.querySelector('.loader');
+
+
+
+window.onload = function() {
+
+loader.classList.remove('loader--active');
+
+};
+
+
+
+document.querySelector('.nav-link').addEventListener('click', function () {
+
+loader.classList.add('loader--active');
+
+
+
+window.setTimeout(function () {
+
+  loader.classList.remove('loader--active');
+
+}, 5000);
+
+});
 
 
 window.addEventListener('scroll', checkVisibility);
