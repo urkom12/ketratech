50ed4910f460f3c9aa93d38498bf9dad15599631
Aleksa Urkom <aleksa@itdevelopment.dev>
2025-04-13 17:18:41 +0200

    hirenow-form css

--- hirenow-form.php
+++ hirenow-form.php
@@ -1,5 +1,5 @@
 <?php include_once('header.php'); ?>
-<link rel="stylesheet" href="hirenow.css">
+<link rel="stylesheet" href="hirenow-form.css">
 
 <div class="main">   
     <video autoplay muted loop playsinline preload="auto" disablePictureInPicture>
